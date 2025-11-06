976a10e11e142b56d70f017a46dba1beb5fb2881
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-06 11:35:26 +0900

    commit

--- chap01-data-structures/src/main/java/com/ohgiraffers/section05/deque/Application.java
+++ chap01-data-structures/src/main/java/com/ohgiraffers/section05/deque/Application.java
@@ -1,8 +1,36 @@
 package com.ohgiraffers.section05.deque;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Application {
 
     public static void main(String[] args) {
+        /*
+        * Java Collection Framework에서는 Deque인터페이스와 이를 구현한 구현체가 있다.
+        * 배열 기반의 동적 큐인 ArrayDeque클래스를 활용한다.
+        * */
+        Deque<Integer> deque = new ArrayDeque<>();
 
+        // 요소 추가
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        deque.addFirst(5);
+
+        System.out.println("deque = " + deque);
+
+        // 요소확인
+        System.out.println(deque.getFirst());
+        System.out.println(deque.getLast());
+
+        // 요소 제거
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+        System.out.println(deque.removeLast());
+
+        System.out.println("deque = " + deque);
     }
 }
--- chap01-data-structures/src/main/java/com/ohgiraffers/section05/deque/Practice1.java
+++ chap01-data-structures/src/main/java/com/ohgiraffers/section05/deque/Practice1.java
@@ -1,4 +1,26 @@
 package com.ohgiraffers.section05.deque;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Practice1 {
+
+    static class Balloon {
+        int order;
+        int noteValue;
+
+        Balloon(int order, int noteValue) {
+            this.order = order;
+            this.noteValue = noteValue;
+        }
+    }
+
+    public String solution(Integer[] notes) {
+
+        StringBuilder sb = new StringBuilder();
+        Deque<Balloon> deque = new ArrayDeque<>();
+
+
+        return sb.toString();
+    }
 }
