d1daf8df7fec91950d558e69d8fa3f21a5074b2c
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-10 10:38:07 +0900

    BruterForceTest 코드 업로드

--- chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/E_BruterForce.java
+++ chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/E_BruterForce.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.section02.searching;
 
-
 /* 완전 탐색(BruteForce) + 백트래킹 (Back Tracking)
  * 주어진 정수들의 모든 순열(순서 있는 조합)을 구하는 문제
  * 가능한 모든 경우의 수를 탐색하는 '완전 탐색' 방식을 사용하지만,
@@ -8,4 +7,16 @@ package com.ohgiraffers.section02.searching;
  * 불필요한 탐색을 줄여 효율적으로 모든 순열의 갯수를 구한다.
  * */
 public class E_BruterForce {
+
+    // 순열이 총 개수를 저장할 변수
+    static int count = 0;
+    // 입력 받은 정수 배열
+    static int[] nums;
+    // 각 정수의 사용 여부를 체크하는 배열
+    static boolean[] visited;
+
+    public static Integer solution(String input){
+
+        return count;
+    }
 }
